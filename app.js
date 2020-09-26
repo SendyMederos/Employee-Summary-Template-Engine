4c7416b54cd0ca514b39de49f050aef6bdf46d9a
SendyMederos <sendymederos@hotmail.com>
2020-09-26 19:58:50 -0400

    adding validation to the inputs on the prompts // optimizing the prompts a variable was create and initialized to optimize th code.

--- app.js
+++ app.js
@@ -15,27 +15,45 @@ const employees = []
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
 
+/// this two functions are to validate that the right type of data is being passed 
+const stringValidation = async function (input) {
+    if (input === null || !isNaN(input)) {
+        return 'this field must be a string';
+    }
+    return true
+}
+const numericValidation = async function (input) {
+    if (input === null || isNaN(input)) {
+        return 'this field must be numeric';
+    }
+    return true
+}
 
-const mgquestions = [
+///this array holds the managers questions and is set to default as we wil always need one manager
+const mgQuestions = [
     {
         type: "input",
         name: "name",
-        message: "Enter the manager's name"
+        message: "Enter the manager's name",
+        validate: stringValidation
     },
     {
         type: "input",
         name: "id",
-        message: "Please enter the employee ID"
+        message: "Please enter the employee ID",
+        validate: numericValidation
     },
     {
         type: "input",
         name: "email",
-        message: "Enter the employee's e-mail"
+        message: "Enter the employee's e-mail",
+        validate: stringValidation
     },
     {
         type: "input",
         name: "office",
-        message: "What is the the mangaer's office number?"
+        message: "What is the the manager's office number?",
+        validate: numericValidation
     },
     {
         type: "confirm",
@@ -43,7 +61,7 @@ const mgquestions = [
         message: "Whould you like to add another employee?"
     },
 ]
-const empquestions = [
+let empQuestions = [
     {
         type: "list",
         name: "typeOfemployee",
@@ -53,87 +71,68 @@ const empquestions = [
     {
         type: "input",
         name: "name",
-        message: "Enter the employee's name"
+        message: "Enter the employee's name",
+        validate: stringValidation
     },
     {
         type: "input",
         name: "id",
-        message: "Please enter the employee's ID"
+        message: "Please enter the employee's ID",
+        validate: numericValidation
     },
     {
         type: "input",
         name: "email",
-        message: "Enter the employee's e-mail"
+        message: "Enter the employee's e-mail",
+        validate: stringValidation
     },
     {
         type: "input",
         name: "github",
         message: "What is the engineer's GitHub?",
-        when: (empquestions) => empquestions.typeOfemployee === "Engineer"
+        validate: stringValidation,
+        when: (empQ) => empQ.typeOfemployee === "Engineer"
     },
     {
         type: "input",
         name: "school",
         message: "What is the intern's school?",
-        when: (empquestions) => empquestions.typeOfemployee === "Intern"
+        validate: stringValidation,
+        when: (empQ) => empQ.typeOfemployee === "Intern"
     },
     {
         type: "confirm",
         name: "addEmployee",
         message: "Whould you like to add another employee?"
     },
-
 ]
 
+/// initializing emq.addEmployee to true to save a few lines of code
+var emq = { addEmployee: true }
+/// this async function is going to execute inquirer with the right set of questions
 async function promptUser() {
-    const mgq = await inquirer.prompt(mgquestions)
+    //first we pas the default set of questions (manager input) and push the object to the employee array 
+    const mgq = await inquirer.prompt(mgQuestions)
     employees.push(new Manager(mgq.name, mgq.id, mgq.email, mgq.office))
-
-    if (mgq.addEmployee === true) {
-        var emq = await inquirer.prompt(empquestions);
+    // looping thru the next set of questions and pushing the objects to the employyes array
+    while (mgq.addEmployee === true && emq.addEmployee === true) {
+        emq = await inquirer.prompt(empQuestions);
         if (emq.typeOfemployee === "Engineer") {
             employees.push(new Engineer(emq.name, emq.id, emq.email, emq.github))
-
         } else { employees.push(new Intern(emq.name, emq.id, emq.email, emq.school)) }
-        while (emq.addEmployee === true) {
-            emq = await inquirer.prompt(empquestions);
-            if (emq.typeOfemployee === "Engineer") {
-                employees.push(new Engineer(emq.name, emq.id, emq.email, emq.github))
-
-            } else { employees.push(new Intern(emq.name, emq.id, emq.email, emq.school)) }
-
-
-        }
     }
 }
+/// this async function will allow to wait for the prompts to execute the render function we imported from render.js
+/// and await for render to execute writeFieAsync 
 async function init() {
     try {
         await promptUser();
-    
         const rendered = await render(employees)
-
         await wirteFileAsync(outputPath, rendered)
     } catch (err) {
         console.log(err)
     }
 }
+// execute init function and let it all happen
 init()
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
 
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
